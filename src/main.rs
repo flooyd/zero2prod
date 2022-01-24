1d74e9c39dea8a2125f44fa6c7f91085a52ab98d
Floyd Jones <floydtjones@gmail.com>
2022-01-24 01:30:52 -0600

    test query

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let listener = TcpListener::bind("127.0.0.1:8080")
         .expect("Failed to bind to port 8080");
     run(listener)?.await
